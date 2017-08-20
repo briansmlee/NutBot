80c0fb915f44d6c8a68748d0c91be3248035f8d0
Brian Lee <briansmlee@gmail.com>
2017-08-20 22:32:25 +0900

    added DRI dict

---
+++ daily_intake.py
@@ -0,0 +1,39 @@
+# DRI: ditary reference intake
+# could not find file online...
+
+dri = { 'Total Fat' : 65, # grams
+        'Saturated Fat' : 20, # grams
+        'Cholesterol' : 300, # milligrams
+        'Sodium' : 2400, #mg
+        'Potassium' : 3500, #mg
+        'Total Carbohydrate' : 300, #g
+        'Dietary Fiber' : 25, #g
+        'Protein' : 50, #g
+        'Vitamin A' : 5000, #IU
+        'Vitamin C' : 60, #mg
+        'Vitamin D' : 60,  #mg
+        'Vitamin E' : 60,  #mg
+        'Vitamin K' : 60,  #mg
+        'Calcium' : 1000, #mg
+        'Iron' : 18, #mg
+        'Thiamin' : 1.5, #mg
+        'Riboflavin' : 1.7, #mg
+        'Niacin' : 20, #mg
+        'Vitamin B6': 2, #mg
+        'Folate' : 400, #ug
+        'Vitamin B12' : 6, #ug
+        'Biotin' : 300, #ug
+        'Pantothenic acid' : 10, #mg
+        'Phosphorus' : 1000, #mg
+        'Iodine' : 150, #ug
+        'Magnesium' : 400, #mg
+        'Zinc' : 15, #mg
+        'Selenium' : 70, #ug
+        'Copper' : 2, #mg
+        'Manganese' : 2, #mg
+        'Chromium' : 120, #ug
+        'Molybdenum' : 75, #ug
+        'Chloride' : 3400 #mg
+        }
+
+
