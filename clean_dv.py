ee8753d9740174f12a5564e57db54275839ff879
Brian Lee <briansmlee@gmail.com>
2017-08-26 15:59:51 +0900

    cleaned daily_values

--- clean_dv.py
+++ clean_dv.py
@@ -1,6 +1,7 @@
 # script to clean usda and daily values csv files and join
 
 import pandas as pd
+import numpy as np
 import csv
 import matplotlib.pyplot as plt
 import chardet
@@ -10,8 +11,26 @@ with open('daily_values.csv', 'rb') as f:
     result = chardet.detect(f.read())
     # print(result, '\n\n\n')
 
-usda_names = ['name', 'daily_values', 'extra'] # specifying to handle inconsistent col nums
-usda = pd.read_csv('daily_values.csv', names=usda_names, encoding=result['encoding'])
-print(usda)
+dv_names = ['name', 'daily_values', 'extra'] # specifying cols to handle inconsistent col nums
+dv = pd.read_csv('daily_values.csv', encoding=result['encoding'], names=dv_names)
+# print(dv)
 
+dv = dv.drop(0) # remove first row due to repeated col names
+# print('\n\n\n', dv) 
+# clean unit by case
+# use if unit is in parens
+
+# usda = pd.DataFrame(usda['daily_values'].str.split(' ', 1).str,
+#        columns = ['quantity', 'unit'])
+
+dv_cleaned = dv['daily_values'].str.split(' ', 1, expand=True) # split dv into quantity, unit
+# print(dv_cleaned)
+dct = { 
+        'name' : dv['name'],
+        'quantity' : dv_cleaned[0],
+        'unit' : dv_cleaned[1]
+    }
+        
+dv = pd.DataFrame(dct)
+print('\n\n\n', dv)
 
