11b192a302e10aa4dc32f6ea3f29e3282e7d4caf
Brian Lee <briansmlee@gmail.com>
2017-08-26 16:56:56 +0900

    cleaning usda csv

--- clean_dv.py
+++ clean_dv.py
@@ -7,34 +7,23 @@ import matplotlib.pyplot as plt
 import chardet
 
 # determining file type due to encoding error
-with open('daily_values.csv', 'rb') as f:
-    result = chardet.detect(f.read())
-    # print(result, '\n\n\n')
+with open('daily_values.csv', 'rb') as f1:
+    result = chardet.detect(f1.read())
 
 dv_names = ['name', 'daily_values', 'extra'] # specifying cols to handle inconsistent col nums
 dv = pd.read_csv('daily_values.csv', encoding=result['encoding'], names=dv_names)
-# print(dv)
-
 dv = dv.drop(0) # remove first row due to repeated col names
-# print('\n\n\n', dv) 
-# clean unit by case
-# use if unit is in parens
-
-# usda = pd.DataFrame(usda['daily_values'].str.split(' ', 1).str,
-#        columns = ['quantity', 'unit'])
 
 dv_cleaned = dv['daily_values'].str.split(' ', 1, expand=True) # split dv into quantity, unit
-# print(dv_cleaned)
 dct = { 
         'name' : dv['name'],
         'quantity' : dv_cleaned[0],
         'unit' : dv_cleaned[1]
     }
-        
 dv = pd.DataFrame(dct)
 
 def clean_unit(unit):
-    if '(' in unit:
+    if '(' in unit: # if unit is in parens, use it
         return unit.split('(')[1].split(')')[0] 
     elif ' ' in unit:
         return unit.split(' ', 1)[1]
@@ -46,5 +35,20 @@ dv['quantity'] = dv['quantity'].apply(lambda x : x.replace(',', ''))
 
 print('\n\n\n', dv)
 
+##### now cleaning usda.csv
 
+with open('usda.csv', 'rb') as f2:
+    result = chardet.detect(f2.read())
+usda = pd.read_csv('usda.csv', encoding=result['encoding']) # names=dv_names)
+print(usda)
+
+def isin_dv(usda_name, dv_names):
+    for name in dv_names:
+        if usda_name.lower() in name: # str pattern matching. NOT finding in list
+            return True
+    return False
+
+# lower for case insensitive comparison
+dv_names = [ name.lower() for name in dv['name'].tolist() ]
+print(usda.loc[isin_dv(usda['name'], dv_names)])
 
