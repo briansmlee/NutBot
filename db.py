135e25df3ea78eb1a62c999957d8081799cb6786
Brian Lee <briansmlee@gmail.com>
2017-08-28 10:24:00 +0900

    normalizing food and consumption

--- db.py
+++ db.py
@@ -6,50 +6,59 @@ import datetime
 from sqlalchemy import *
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, mapper, session
+import csv
 
-fmt = "%Y-%m-%d %H:%M:%S %Z"
+fmt = "%Y-%m-%d %H:%M:%S %Z" #dateTime format
 
-engine = create_engine('sqlite:///ver1.db')
-
-Base = declarative_base() 
-
+# session connects to engine for resources
+engine = create_engine('sqlite:///ver2.db') 
 # create session
 Session = sessionmaker(bind=engine)
 session = Session()
-
-
-##### Instead of hard-coding columns, go dynamic
-# class Food(Base):
-#     __tablename__ = 'food'
-#     
-#     id = Column(Integer, primary_key = True)
-#     date_time = Column(DateTime)
-#     name = Column(String)
-#     quantity = Column(Integer)
-#     calories = Column(Float)
-#     user = Column(String)
-#     
-#     # optional
-#     def __repr__(self):
-#         return "<Food: datetime=%s name=%s, quantity=%d, calories=%f user=%s>" % \
-#                 (self.date_time.strftime(fmt), self.name, self.quantity, self.calories, self.user)
-# 
-
-##### dynamic 
-class Food(Base):
-    pass
-    # __tablename__ = 'food'
-
+# use declarative base
+Base = declarative_base() 
 metadata = MetaData(bind=engine)
 
-t = Table('foods', metadata,
-        Column('id', Integer, primary_key=True)
-        *(Column(foodName, type_table[foodName]) for foodName in cols))
+class Consumption(Base):
+    __tablename__ = 'Consumption'
+     
+    id = Column(Integer, primary_key = True)
+    date_time = Column(DateTime) # when ate
+    user = Column(String) # who ate
+    food_name = Column(String) 
+    food_id = Column(String)
+    quantity = Column(Integer)
+    calories = Column(Float) # move to food table?
+     
+    # optional
+    # def __repr__(self):
+    #    return "<Food: datetime=%s name=%s, quantity=%d, calories=%f user=%s>" % \
+                
 
-metadata.create_all()
-mapper(Food, t)
-session = create_session(bind=engine, autocommit=False, autoFlush=True)
-Base.metadata.create_all(engine)
+##### mapping food table to class
+
+class Food(object):
+    # def __init__(self, food_dct):
+    pass
+
+dv_file = open('./dv.csv')
+dvReader = csv.DictReader(dv_file)
+for n in dvReader:
+    print(n)
+
+food_table = Table('food', metadata,
+        Column('id', Integer, primary_key=True),
+        Column('food_id', String),
+        # dynamic column generation from daily value columns
+        # use attr_id or name?
+        *(Column(nutrition['name'], Integer) for nutrition in dvReader)
+        )
+
+
+metadata.create_all() # creates table
+mapper(Food, food_table)
+# session = create_session(bind=engine, autocommit=False, autoFlush=True)
+# Base.metadata.create_all(engine)
 
 def db_add_food(dct, user):
     dct['user'] = user
@@ -59,7 +68,6 @@ def db_add_food(dct, user):
     
 def db_all_foods(users):
     """get all rows rel to input users"""
-
     # for instance in session.query(Food).order_by(Food.id):
     #    print(instance) # ok?
 
